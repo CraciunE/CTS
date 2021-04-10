9a2c8e2b612bc2f83dee9d5b6f4f93b5003d5134
CraciunE <craciunelena18@stud.ase.ro>
2021-04-10 15:13:54 +0300

    Composite

---
+++ Seminar7-CTS/src/ro/ase/cts/composite/Main.java
@@ -0,0 +1,25 @@
+package ro.ase.cts.composite;
+
+import javax.crypto.spec.ChaCha20ParameterSpec;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Agentie a1= new Agentie("a1");
+		Agentie a2= new Agentie("a2");
+		
+		Filiala f1= new Filiala("f1");
+		Filiala f2= new Filiala("f2");
+		Filiala f3= new Filiala("f3");
+		a1.adaugaSediu(f1);
+		a2.adaugaSediu(f2);
+		a2.adaugaSediu(f3);
+		
+		a1.adaugaSediu(a1);
+		
+		a1.descriere();
+
+	}
+
+}
