8e87e18a9e818547ebe0ecd4a0fe43903d77dfa7
CraciunE <craciunelena18@stud.ase.ro>
2021-05-22 15:06:28 +0300

    Test Unit 4

--- Seminar12/src/ro/ase/cts/StudentTests.java
+++ Seminar12/src/ro/ase/cts/StudentTests.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import javax.swing.plaf.basic.BasicGraphicsUtils;
 
 import org.junit.Test;
+import org.junit.experimental.categories.Categories.ExcludeCategory;
 
 public class StudentTests {
 
@@ -12,28 +13,28 @@ public class StudentTests {
 	public void testConstructorCuParametru() {
 		String nume = "Gigel";
 		Student student = new Student(nume);
-		
+
 		assertEquals(nume, student.getNume());
 	}
-	
+
 	@Test
 	public void testCunstructorFaraParametru() {
 		Student student = new Student();
-		
-		//assertNotEquals(null,student.getNume());
-		assertNotNull("Test nume",student.getNume());
-		assertNotNull("Test note",student.getNote());
+
+		// assertNotEquals(null,student.getNume());
+		assertNotNull("Test nume", student.getNume());
+		assertNotNull("Test note", student.getNote());
 	}
-	
+
 	@Test
 	public void testAdaugaNota() {
 		Student student = new Student();
 		int nota = 10;
 		student.adaugaNota(nota);
 		assertEquals(nota, student.getNota(0));
-		
-}
-	
+
+	}
+
 	@Test
 	public void testDimensiuneLista() {
 		Student student = new Student();
@@ -41,20 +42,20 @@ public class StudentTests {
 		student.adaugaNota(nota);
 		assertEquals(1, student.getNote().size());
 	}
-	
+
 	@Test
 	public void testCalculeazaMedie() {
 		Student student = new Student();
 		float nota1 = 5;
 		float nota2 = 10;
-		student.adaugaNota((int)nota1);
-		student.adaugaNota((int)nota2);
-		float medie =  student.calculeazaMedie();
-		float medieCalculata = (nota1+nota2)/2;
-		
+		student.adaugaNota((int) nota1);
+		student.adaugaNota((int) nota2);
+		float medie = student.calculeazaMedie();
+		float medieCalculata = (nota1 + nota2) / 2;
+
 		assertEquals(medieCalculata, medie, 0.001);
 	}
-	
+
 	@Test
 	public void testAreRestanteTrue() {
 		Student student = new Student();
@@ -62,7 +63,6 @@ public class StudentTests {
 		student.adaugaNota(10);
 		assertTrue(student.areRestante());
 	}
-	
 
 	@Test
 	public void testAreRestanteFalse() {
@@ -72,5 +72,49 @@ public class StudentTests {
 		assertFalse(student.areRestante());
 	}
 
+	@Test
+	public void testSetNume() {
+		Student student = new Student();
+		String nume = "Dorel";
+		student.setNume(nume);
+		assertEquals(nume, student.getNume());
+	}
 
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void testGetNotaAruncaExceptie() {
+		Student student = new Student();
+		student.getNota(-1);
+	}
+
+	@Test
+	public void testGetNotaAruncaExceptieJUnit3() {
+		Student student = new Student();
+		// 1
+		try {
+			// 2
+			student.getNota(-1);
+			// 3 - nu este executata
+			fail("Metoda nu arunca nicio exceptia");
+		} catch (IndexOutOfBoundsException e) {
+			// 4
+		} catch (Exception e) {
+			// 5 - nu este executata
+			fail("Nu este aruncata o exceptie de tipul IndexOutOfBoundsException");
+		}
+		// 6
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testAdaugaNoteNegative() {
+		Student student = new Student();
+		student.adaugaNota(-10);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testAdaugaNotePreaMari() {
+		Student student = new Student();
+		student.adaugaNota(100);
+	}
 }
+
+
