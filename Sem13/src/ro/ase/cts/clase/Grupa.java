44c06293f4815150d1c1e09af6f60258df95dc85
CraciunE <craciunelena18@stud.ase.ro>
2021-05-29 14:58:23 +0300

    JunitTests

---
+++ Sem13/src/ro/ase/cts/clase/Grupa.java
@@ -0,0 +1,63 @@
+package ro.ase.cts.clase;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Grupa {
+	private List<IStudent> studenti;
+    private int nrGrupa;
+
+    public Grupa(int nrGrupa){
+    	if(nrGrupa>=1000 && nrGrupa<=1100) {
+    		 this.nrGrupa=nrGrupa;
+    	}else {
+    		throw new IllegalArgumentException();
+    	}
+        studenti=new ArrayList<>();
+    }
+
+    public void importaStudenti(List<IStudent> studenti){
+        this.studenti.addAll(studenti);
+    }
+
+    public void adaugaStudent(IStudent student){
+        studenti.add(student);
+    }
+
+    public IStudent getStudent(int index){
+        if(index>=0 && index< studenti.size()){
+            return studenti.get(index);
+        }
+        throw new IndexOutOfBoundsException();
+    }
+
+    public float getPromovabilitate(){
+        if(studenti.size() ==0) {
+        	return 0;
+        }
+        int nrRestantieri=0;
+        for(IStudent student:studenti){
+            if(student.areRestante()){
+                nrRestantieri++;
+            }
+        }
+        return (studenti.size() -nrRestantieri)/(float)studenti.size();
+    }
+
+	public List<IStudent> getStudenti() {
+		return studenti;
+	}
+
+	public void setStudenti(List<IStudent> studenti) {
+		this.studenti = studenti;
+	}
+
+	public int getNrGrupa() {
+		return nrGrupa;
+	}
+
+	public void setNrGrupa(int nrGrupa) {
+		this.nrGrupa = nrGrupa;
+	}
+    
+    
+}
